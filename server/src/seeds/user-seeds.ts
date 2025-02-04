ef5d0e58c9418cab8eb34e0427fa25f6eb37fdcc
Nathan Green <ngreen1632@gmail.com>
2025-02-04 14:50:02 -0600

    Revert "Merge pull request #36 from nathangreen1632/snabaj/userSeeds"
    This reverts commit 8d44c4eb24aa725cabebad4a5ce873617d27f5e7, reversing
    changes made to 509325265ff305ab036813153cdb4a268e72bbef.

--- server/src/seeds/user-seeds.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../models/user.js';
-
-export const seedUsers = async () => {
-    await User.bulkCreate([
-        { username: 'BeanBag', password: 'password' },
-        { username: 'MuffinTop', password: 'password' },
-        { username: 'PrimeGuru', password: 'password' },
-    ], { individualHooks: true });
-};
