1ebd86ff0e6b013c982286f6e346db47aff8be61
Nathan Green <ngreen1632@gmail.com>
2025-02-04 14:51:41 -0600

    Revert "Merge pull request #32 from nathangreen1632/CC-reactdev1"
    This reverts commit 023679fb57ded961f8782c9bb700a686e468bd4a, reversing
    changes made to 65f8cd0fbcbdabd69b2159ac3dc0103443deeae0.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,35 @@
-//Bringing in the required import from 'react-router-dom'
-//import { useState } from 'react'
-//import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
 
 function App() {
-  // The Outlet component will conditionally swap between the different pages according to the URL
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Navbar />
-      <main className="mx-3">
-        <Outlet />
-      </main>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
