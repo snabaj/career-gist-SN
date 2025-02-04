89bee4471df7a0e21e85b732c43fcbe8e5f5489e
Nathan Green <ngreen1632@gmail.com>
2025-02-04 14:51:05 -0600

    Revert "Merge pull request #34 from nathangreen1632/CC-reactdev2"
    This reverts commit 1a3626b311f96ebb750851217544ffd599abb1a9, reversing
    changes made to 1d2f62b71940b3756c2d976953b1c29945aef5cc.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,40 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Error from './pages/Error';
-import HomePage from './pages/HomePage';
-import SavedJobs from './pages/SavedJobs';
-import AppliedTo from './pages/AppliedTo';
-import SignUp from './pages/SignUp';
-import LogIn from './pages/LogIn';
-import Footer from './pages/Footer';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-// Get the root element
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
-
-// Create the root
-const root = createRoot(rootElement);
-
-// Render the application within the StrictMode and BrowserRouter
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
-          <Route path="SavedJobs" element={<SavedJobs />} />
-          <Route path="AppliedTo" element={<AppliedTo />} />
-          <Route path="SignUp" element={<SignUp />} />
-          <Route path="LogIn" element={<LogIn />} />
-          <Route path="Footer" element={<Footer />} />
-          {/* Handling Error or Not Found Cases */}
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
-
+    <App />
+  </StrictMode>,
+)
