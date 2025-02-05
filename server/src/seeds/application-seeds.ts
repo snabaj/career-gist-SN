a896ce10596eefbf98b5628c99687f6969bba736
Star <snabajja@gmail.com>
2025-02-04 20:58:52 -0500

    build: add code to application-seeds file

--- server/src/seeds/application-seeds.ts
+++ server/src/seeds/application-seeds.ts
@@ -1,4 +1,27 @@
-import { Application } from '../models/applicationsModel';
+import { Application } from '../models/applicationsModel.js';
 
 export const seedApplications = async () => {
-    
+    await Application.bulkCreate([
+        {
+            job_id: 1,
+            applied: true,
+            date_applied: new Date(),
+            attached_cover_letter: true,
+            status: 'Pending',
+        },
+        {
+            job_id: 2,
+            applied: false,
+            date_applied: new Date(),
+            attached_cover_letter: false,
+            status: 'No Response',
+        },
+        {
+            job_id: 3,
+            applied: true,
+            date_applied: new Date(),
+            attached_cover_letter: true,
+            status: 'Accepted',
+        },
+    ]);
+};
