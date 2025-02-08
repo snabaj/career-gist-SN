9ef4bff97a2c27a0b11116504e5bece3cbbae2c8
Star <snabajja@gmail.com>
2025-02-07 22:22:40 -0500

    chore: remove comments and unused code from model/index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,17 +1,1 @@
-import sequelize from '../config/connection.js';
-import {User} from "./userModel";
 
-
-
-
-// User.hasMany(Ticket, { foreignKey: 'assignedUserId' }); !EXAMPLE - not real code!
-// Setup associations
-
-// export User work and applications
-
-
-
-
-
-
-export default { sequelize, User };
