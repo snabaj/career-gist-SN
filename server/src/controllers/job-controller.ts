f8fd51b6f8e2b59ebd4db2ce5a6d8ec1208c226b
Star <snabajja@gmail.com>
2025-02-10 16:38:53 -0500

    refactor: renamed job position to 'title' in jobModel and job-seeds files

---
+++ server/src/controllers/job-controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+import { Job } from '../models/jobsModel.js';
+import { User } from '../models/userModel.js';
+
+// GET /jobs
+export const getAllJobs = async (_req: Request, res: Response) => {
+  try {
+    const jobs = await Job.findAll({
+        include: [
+            {
+                model: User,
+                as: 'user',
+                attributes: ['username'],
+            }
+        ]
+    });
+    res.json(jobs);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET /jobs/:id
+export const getJobById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const job = await Job.findByPk(id, {
+        include: [
+            {
+                model: User,
+                as: 'user',
+                attributes: ['username'],
+            }
+        ]
+    });
+    if (job) {
+      res.json(job);
+    } else {
+      res.status(404).json({ message: 'Job not found' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// POST /jobs
+export const createJob = async (req: Request, res: Response) => {
+  const { title, description, userId } = req.body;
+  try {
+    const newJob = await Job.create({ title, description, userId });
+    res.status(201).json(newJob);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
