573158fa5370c18a3097b0ba5d0538821591e3d8
Star <snabajja@gmail.com>
2025-02-10 17:14:24 -0500

    build: add jobRouter to api/index.ts file

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './userRoutes.js';
+import { jobRouter } from './jobs.js';
 
 const router : Router = Router();
 
 router.use('/users', userRouter);
+router.use('/jobs', jobRouter);
 
 export default router;
