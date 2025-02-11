ed51b639d8598195a7967c49b25329a39a6b5974
Star <snabajja@gmail.com>
2025-02-11 12:51:28 -0500

    build: import authRoutes into routes/index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import favoritesRoutes from "./favorites.js";
 import jobsRoutes from "./jobsRoutes.js";
 import apiRoutes from "./api/index.js";
+import authRoutes from "./auth-routes.js"
 
 const router : Router = Router();
 
 router.use("/favorites", favoritesRoutes);
 router.use("/jobs", jobsRoutes);
+router.use("/auth", authRoutes);
 router.use("/", apiRoutes);
 
 export default router;
