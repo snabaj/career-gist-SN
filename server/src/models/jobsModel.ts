37045bf68b88925a2554760ba2bd6bea72aec806
Star <snabajja@gmail.com>
2025-02-12 00:56:59 -0500

    refactor: recreated jobsModel to match jobDetails interface

--- server/src/models/jobsModel.ts
+++ server/src/models/jobsModel.ts
@@ -1,61 +1,96 @@
 import { DataTypes, Sequelize, Model, Optional } from "sequelize";
 
 interface JobAttributes {
-  id: number;
-  title: string;
-  description: string;
-  remote_onsite: 'Remote' | 'Onsite' | 'Hybrid';
-  salary: string;
-  date_published: Date;
-  experience_level: string;
-  company_id: number;
+  job_id: string;
+  job_title: string;
+  employer_name: string;
+  job_publisher: string;
+  job_employment_type: string;
+  job_apply_link: string;
+  job_description: string;
+  job_is_remote: boolean;
+  job_posted_at: string;
+  job_location: string;
+  job_city?: string;
+  job_state?: string;
+  job_country?: string;
   saved?: boolean;
 }
 
-interface JobCreationAttributes extends Optional<JobAttributes, "id"> {}
+interface JobCreationAttributes extends Optional<JobAttributes, "job_id"> {}
 
 export class Job extends Model<JobAttributes, JobCreationAttributes> implements JobAttributes {
-  public id!: number;
-  public title!: string;
-  public description!: string;
-  public remote_onsite!: 'Remote' | 'Onsite' | 'Hybrid';
-  public salary!: string;
-  public date_published!: Date;
-  public experience_level!: string;
-  public company_id!: number;
+  public job_id!: string;
+  public job_title!: string;
+  public employer_name!: string;
+  public job_publisher!: string;
+  public job_employment_type!: string;
+  public job_apply_link!: string;
+  public job_description!: string;
+  public job_is_remote!: boolean;
+  public job_posted_at!: string;
+  public job_location!: string;
+  public job_city?: string;
+  public job_state?: string;
+  public job_country?: string;
+  public saved?: boolean;
 }
 export function JobFactory(sequelize: Sequelize): typeof Job {
   Job.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
+    job_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
       primaryKey: true,
     },
-    title: {
+    job_title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
+    employer_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    job_publisher: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    job_employment_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    job_apply_link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    job_description: {
       type: DataTypes.TEXT,
-    },
-    remote_onsite: {
-      type: DataTypes.ENUM('Remote', 'Onsite', 'Hybrid'),
       allowNull: false,
     },
-    salary: {
+    job_is_remote: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    job_posted_at: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    job_location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    job_city: {
       type: DataTypes.STRING,
     },
-    date_published: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    experience_level: {
+    job_state: {
       type: DataTypes.STRING,
     },
-    company_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+    job_country: {
+      type: DataTypes.STRING,
+    },
+    saved: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   },
   {
@@ -63,6 +98,6 @@ export function JobFactory(sequelize: Sequelize): typeof Job {
     tableName: "jobs",
   }
 );
-return Job;
-};
 
+return Job;
+}
