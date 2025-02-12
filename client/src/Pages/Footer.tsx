4564689f4ba562894ed59e2c65785c3ad6610849
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-02-12 00:46:22 -0500

    made sure that the links open to a new tab

--- client/src/Pages/Footer.tsx
+++ client/src/Pages/Footer.tsx
@@ -5,10 +5,10 @@ import github from '../assets/github-mark.png'; // Ensure the path is correct
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <a href="https://github.com/nathangreen1632/career-gist">
+      <a href="https://github.com/nathangreen1632/career-gist" target="_blank" rel="noopener noreferrer">
        <img className={styles.githublogo} src={github} alt="Career Gist Logo" />
       </a>
-      <p>CareerGist. Copyright © 2025. || <a className={styles.footerLink} href= "https://github.com/nathangreen1632">Nathan Green,</a> <a className={styles.footerLink} href="https://github.com/ccasalme">Cyrl Casalme,</a> <a className={styles.footerLink} href="https://github.com/snabaj">Stella Nabajja</a></p>
+      <p>CareerGist. Copyright © 2025. || <a className={styles.footerLink} href= "https://github.com/nathangreen1632" target="_blank" rel="noopener noreferrer">Nathan Green,</a> <a className={styles.footerLink} href="https://github.com/ccasalme" target="_blank" rel="noopener noreferrer">Cyrl Casalme,</a> <a className={styles.footerLink} href="https://github.com/snabaj" target="_blank" rel="noopener noreferrer">Stella Nabajja</a></p>
     </footer>
   );
 };
