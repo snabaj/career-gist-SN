d28c5a0805a9c0abf0cb69153ab9aac9d975e0a9
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-02-11 23:19:31 -0500

    put back the jobsearch to the way it was

--- client/src/types/interface/jobSearch.ts
+++ client/src/types/interface/jobSearch.ts
@@ -1,69 +1,19 @@
-// export interface JobSearchResponse {
-//   success: boolean;
-//   data: JobSearchData;
-// }
-
-// export interface JobSearchData {
-//   status: string;
-//   request_id: string;
-//   parameters?: JobSearchParameters;
-//   data: JobDetails[];
-// }
-
-// export interface JobSearchParameters {
-//   query?: string;
-//   page?: number;
-//   num_pages?: number;
-// }
-
-// export interface JobDetails {
-//   job_id: string;
-//   job_title: string;
-//   employer_name: string;
-//   job_publisher: string;
-//   job_employment_type: string;
-//   job_apply_link: string;
-//   job_description: string;
-//   job_is_remote: boolean;
-//   job_posted_at: string;
-//   job_location: string;
-//   job_city?: string;
-//   job_state?: string;
-//   job_country?: string;
-//   job_highlights?: JobHighlights;
-// }
-
-// export interface JobHighlights {
-//   Qualifications?: string[];
-//   Benefits?: string[];
-//   Responsibilities?: string[];
-// }
-
 export interface JobSearchResponse {
-  data: JobSearchData[];
+  success: boolean;
+  data: JobSearchData;
 }
 
 export interface JobSearchData {
-  job_id: string;
-  job_title: string;
-  employer_name: string;
-  job_publisher: string;
-  job_location: string;
-  job_description: string;
-  job_employment_type: string;
-  job_apply_link: string;
-  job_highlights?: JobHighlights;
-  job_is_remote: boolean;
-  job_posted_at: string;
-  job_city: string;
-  job_state: string;
-  job_country: string;
+  status: string;
+  request_id: string;
+  parameters?: JobSearchParameters;
+  data: JobDetails[];
 }
 
-export interface JobHighlights {
-  Qualifications?: string[];
-  Responsibilities?: string[];
-  Benefits?: string[];
+export interface JobSearchParameters {
+  query?: string;
+  page?: number;
+  num_pages?: number;
 }
 
 export interface JobDetails {
@@ -71,14 +21,21 @@ export interface JobDetails {
   job_title: string;
   employer_name: string;
   job_publisher: string;
-  job_location: string;
-  job_description: string;
   job_employment_type: string;
   job_apply_link: string;
-  job_highlights?: JobHighlights;
+  job_description: string;
   job_is_remote: boolean;
   job_posted_at: string;
-  job_city: string;
-  job_state: string;
-  job_country: string;
+  job_location: string;
+  job_city?: string;
+  job_state?: string;
+  job_country?: string;
+  job_highlights?: JobHighlights;
 }
+
+export interface JobHighlights {
+  Qualifications?: string[];
+  Benefits?: string[];
+  Responsibilities?: string[];
+}
+
