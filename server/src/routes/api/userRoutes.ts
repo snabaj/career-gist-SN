23b0ccd30de547ad8e81c789d5b78748b9172be2
Star <snabajja@gmail.com>
2025-02-12 22:05:46 -0500

    chore: delete comments and clean up userRoutes.ts

--- server/src/routes/api/userRoutes.ts
+++ server/src/routes/api/userRoutes.ts
@@ -9,19 +9,14 @@ import {
 
 const router = express.Router();
 
-// GET /users - Get all users
 router.get('/', getAllUsers);
 
-// GET /users/:id - Get a user by id
 router.get('/:id', getUserById);
 
-// POST /users - Create a new user
 router.post('/', createUser);
 
-// PUT /users/:id - Update a user by id
 router.put('/:id', updateUser);
 
-// DELETE /users/:id - Delete a user by id
 router.delete('/:id', deleteUser);
 
 export { router as userRouter };
